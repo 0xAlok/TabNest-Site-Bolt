cb1a7c718e5751c883af23edfdbba7eebbbb283c
Alok <70086704+0xAlok@users.noreply.github.com>
2025-05-25 01:26:57 +0530

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // No specific Astro configurations needed for this simple static site yet.
+  // If deploying to a subpath, you might need to set `site` and `base` options.
+});
